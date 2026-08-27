295f21c8ce2425dbe9a0b678a041994fd8836e4f
Mohammad Arman <233636922+arman77887@users.noreply.github.com>
2026-08-27 23:15:03 +0300

    Create queue.php

---
+++ backend/config/queue.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'default' => env('QUEUE_CONNECTION', 'database'),
+    'connections' => [
+        'sync' => [
+            'driver' => 'sync',
+        ],
+        'database' => [
+            'driver' => 'database',
+            'table' => 'jobs',
+            'queue' => 'default',
+            'retry_after' => 90,
+            'after_commit' => false,
+        ],
+    ],
+    'batching' => [
+        'database' => env('DB_CONNECTION', 'pgsql'),
+        'table' => 'job_batches',
+    ],
+    'failed' => [
+        'driver' => env('QUEUE_FAILED_DRIVER', 'database-uuids'),
+        'database' => env('DB_CONNECTION', 'pgsql'),
+        'table' => 'failed_jobs',
+    ],
+];
