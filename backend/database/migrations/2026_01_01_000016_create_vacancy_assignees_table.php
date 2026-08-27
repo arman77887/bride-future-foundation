6dc43364e8c267a5781ed96f0664133c847063df
Mohammad Arman <233636922+arman77887@users.noreply.github.com>
2026-08-27 23:27:24 +0300

    Create migration for vacancy_assignees table

---
+++ backend/database/migrations/2026_01_01_000016_create_vacancy_assignees_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('vacancy_assignees', function (Blueprint $table) {
+            $table->uuid('vacancy_id');
+            $table->uuid('user_id');
+
+            $table->primary(['vacancy_id', 'user_id']);
+            $table->foreign('vacancy_id')->references('id')->on('vacancies')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('vacancy_assignees');
+    }
+};
