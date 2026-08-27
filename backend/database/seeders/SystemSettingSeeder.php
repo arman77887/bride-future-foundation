d2741b852a163cfc426212b5ef9d1e64f917a3ef
Mohammad Arman <233636922+arman77887@users.noreply.github.com>
2026-08-27 23:49:20 +0300

    Add SystemSettingSeeder for initial settings

---
+++ backend/database/seeders/SystemSettingSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\SystemSetting;
+
+class SystemSettingSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $settings = [
+            ['key' => 'site_name_bn', 'value' => 'ব্রাইড ফিউচার ফাউন্ডেশন', 'type' => 'string', 'group' => 'general', 'is_public' => true],
+            ['key' => 'site_name_en', 'value' => 'Bride Future Foundation', 'type' => 'string', 'group' => 'general', 'is_public' => true],
+            ['key' => 'contact_email', 'value' => 'info@bff.org.bd', 'type' => 'string', 'group' => 'contact', 'is_public' => true],
+            ['key' => 'maintenance_mode', 'value' => 'false', 'type' => 'boolean', 'group' => 'system', 'is_public' => false],
+        ];
+
+        foreach ($settings as $setting) {
+            SystemSetting::updateOrCreate(['key' => $setting['key']], $setting);
+        }
+    }
+}
