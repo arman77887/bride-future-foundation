9eadbbe72544a2a941998b01cb697bf5fd966a83
Mohammad Arman <233636922+arman77887@users.noreply.github.com>
2026-08-27 23:47:13 +0300

    Create SystemSetting model with fillable attributes

---
+++ backend/app/Models/SystemSetting.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SystemSetting extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'key',
+        'value',
+        'type',
+        'group',
+        'description',
+        'is_public',
+    ];
+
+    protected $casts = [
+        'is_public' => 'boolean',
+    ];
+}
