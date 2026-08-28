1159a634ade0db94e92cd64679937302714e9986
Mohammad Arman <233636922+arman77887@users.noreply.github.com>
2026-08-28 04:33:02 +0300

    Create Project.php

---
+++ backend/app/Models/Project.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'title_bn',
+        'title_en',
+        'slug',
+        'description_bn',
+        'description_en',
+        'status',
+    ];
+}
